c396a888cb2c36a9cd7fa9a69dbb0121c15b134e
sarthakbag30 <58979606+sarthakbag30@users.noreply.github.com>
2022-09-08 09:41:41 +0530

    Final Commit

--- stringCalculator.cpp
+++ stringCalculator.cpp
@@ -13,24 +13,45 @@ class StringCalculator{
     }
     
     vector<int> nums;
-    int x=0;
+    int x=0,t=0;
     for(int i=0;i<numbers.length();i++){
     if(numbers[i]==','){
+        if(t) x*=-1;
         nums.push_back(x);
         x=0;
+        t=0;
     }
-    else if(numbers[i]>='1' and numbers[i]<='9'){
+    else if(numbers[i]=='-'){
+        t=1;
+    }
+    else if(numbers[i]>='0' and numbers[i]<='9'){
         int y = numbers[i]-'0';
         x*=10;
         x+=y;
     }
     else if(numbers[i]>='a' and numbers[i]<='z'){
-        int y = numbers[i]-'a';
+        int y = numbers[i]-'a'+1;
         x=y;
     }
     }
     nums.push_back(x);
-    
+    try {
+        int flag=0;
+        for(auto x:nums){
+            if(x<0) flag=1;
+        }
+       if (flag==1)
+       {
+        throw nums;
+       }
+    }
+    catch(vector<int> t){
+        cout<<"Negatives not allowed ";
+        for(auto x:t){
+            if(x<0) cout<<x<<' ';
+        }
+        cout<<endl;
+    }
     for(auto x:nums){
         if(x<=1000) res+=x;
     }
@@ -45,7 +66,5 @@ class StringCalculator{
 int main()
 {
     StringCalculator s;
-    cout<<s.add("12,8");
-
     return 0;
 }
